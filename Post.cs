e345284adfa3591f119e9a5bce08e1da1f4d90fc
Luke Franczek <slfranczek@gmail.com>
2024-07-21 16:15:40 -0400

    initial, backend start

---
+++ Post.cs
@@ -0,0 +1,11 @@
+namespace Moderation;
+public class Post{
+    public int Id { get; set; }
+    public int UserId {get; set;}
+    required public string Title { get; set; }
+    required public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsReviewed { get; set; }
+    public bool IsVisible { get; set; }
+    public string? ReviewNotes {get; set;}
+}
